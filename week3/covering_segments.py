42e4f7980a9023c57e5905435d2cd4c183203f32
dinesh ketumakha <dinesh.ketumakha@gmail.com>
2023-07-26 09:54:46 -0700

    final solution to signatures problem

--- week3/covering_segments.py
+++ week3/covering_segments.py
@@ -3,28 +3,39 @@ from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
+def matches_previous_segments(left, matching_segments):
+    for seg in matching_segments:
+        if left < seg.start or left > seg.end:
+            return False
+    return True
+
+
 def optimal_points(segments):
     if not segments:
         return    
     points = []
     index = 0
     segments = sorted(segments,)
-    valid_index = -1
 
     # write your code here
     while index < len(segments):
         ref_segment = segments[index]
+        # segments whose left end falls within the ref segment's width
+        matches = []
 
-        for i in range(index, len(segments)):
-            current_segment = segments[i]
-            
-            if current_segment.start >= ref_segment.start and current_segment.start <= ref_segment.end:
-                valid_index = i
+        while index < len(segments):
+            current_segment = segments[index]
+            if (
+                current_segment.start >= ref_segment.start and 
+                current_segment.start <= ref_segment.end and
+                matches_previous_segments(current_segment.start, matches)
+            ):
+                matches.append(current_segment)
+                index += 1
             else:
                 break
-
-        points.append(segments[valid_index].start)
-        index = valid_index + 1
+        
+        points.append(segments[index-1].start)
     return points
 
 
