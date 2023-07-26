eec92720281c80dc5e7547aa08714cd5b31e8796
dinesh ketumakha <dinesh.ketumakha@gmail.com>
2023-07-25 23:10:54 -0700

    added more tests

--- tests/test_week3.py
+++ tests/test_week3.py
@@ -1,6 +1,8 @@
 from week3.change_1 import get_change
 from week3.car_fueling_3 import *
 from week3.dot_product import max_dot_product, max_dot_product_naive
+from week3.covering_segments import Segment, optimal_points
+
 from time import time
 
 def test_get_change():
@@ -42,3 +44,74 @@ def test_max_dot_product_large():
     fs = [1, 2, 3, 4]
     ss = [x for x in range(1, 11)]
     max_dot_product(fs, ss)
+
+def test_optimal_points_failed_test():
+    segments = [
+        Segment(1, 3),
+        Segment(2, 5),
+        Segment(3, 6),
+    ]
+    points = optimal_points(segments)
+    assert points == [3]
+
+def test_optimal_points():
+    segments = [
+        Segment(6, 8),
+        Segment(2, 5),
+        Segment(1, 3),
+        Segment(10, 11),
+        Segment(7, 9),
+    ]
+    points = optimal_points(segments)
+    assert len(points) == 3
+    assert points == [2, 7, 10]
+
+    assert optimal_points([]) is None
+
+def test_optimal_points_two_segments():
+    segments = [
+        Segment(3, 5),
+        Segment(1, 2),
+    ]
+    points = optimal_points(segments)
+    assert points == [1, 3]
+
+def test_optimal_points_one_segment():
+    segments = [
+        Segment(3, 5),
+    ]
+    points = optimal_points(segments)
+    assert points == [3]
+
+def test_optimal_points_large_input_failed_test():
+    import os
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    fname = os.path.join(dirname, 'segment_data')
+    segments = []
+
+    with open(fname, 'r') as data_file:
+        for line in data_file.readlines():
+            start,end = line.rstrip("\n").split(" ")
+            segments.append(Segment(int(start), int(end)))
+    
+    segments = sorted(segments)
+    # for i, seg in enumerate(segments):
+    #     print(i, " ", seg.start, "  ", seg.end)
+    
+    points = optimal_points(segments)
+    print(len(points))
+    print(points)
+
+def test_optimal_points_point_segment():
+    segments = [
+        Segment(80, 82),
+        Segment(81, 81),
+        Segment(82, 83)
+    ]
+    points = optimal_points(segments)
+    assert points == [81, 82]
+
+
+
+
+    
--- week3/covering_segments.py
+++ week3/covering_segments.py
@@ -4,11 +4,27 @@ from collections import namedtuple
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
+    if not segments:
+        return    
     points = []
+    index = 0
+    segments = sorted(segments,)
+    valid_index = -1
+
     # write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    while index < len(segments):
+        ref_segment = segments[index]
+
+        for i in range(index, len(segments)):
+            current_segment = segments[i]
+            
+            if current_segment.start >= ref_segment.start and current_segment.start <= ref_segment.end:
+                valid_index = i
+            else:
+                break
+
+        points.append(segments[valid_index].start)
+        index = valid_index + 1
     return points
 
 
